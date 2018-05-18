dcadd8f14e35dd7e2874608486ecfa2a4504f250
Eduardo Acosta Miguens <eduardo.miguens@mercadolibre.com>
2018-05-18 17:11:42 -0300

    [MPCSSR-466] Change Gordics JSON schema lib to gojsonschema
    gojsonschema has more detailed error reporting, which is helpful for returning each field that failed validation and under what condition.

--- gk/jsonschema/jsonschema.go
+++ gk/jsonschema/jsonschema.go
@@ -1,22 +1,55 @@
 package jsonschema
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
 	"path"
+	"strings"
 	"sync"
 
-	"github.com/santhosh-tekuri/jsonschema"
+	"github.com/xeipuuv/gojsonschema"
 )
 
 // ValidationError is the error type returned by Validate.
-type ValidationError = jsonschema.ValidationError
+type ValidationError struct {
+	Schema string
+	Errors []gojsonschema.ResultError
+}
+
+// ErrorsDescription returns a map that contains JSON attribute paths as keys,
+// and an error description detailing what the error cause was.
+func (v ValidationError) ErrorsDescription() map[string]string {
+	errors := map[string]string{}
+
+	for _, err := range v.Errors {
+		field := err.Field()
+		if err.Type() == "required" {
+			field = fmt.Sprintf("%s.%s", err.Context().String(), err.Field())
+			field = strings.Replace(field, "(root).", "", 1)
+		}
+
+		errors[field] = err.Description()
+	}
+
+	return errors
+}
+
+func (v ValidationError) Error() string {
+	buf := bytes.NewBuffer(nil)
+
+	for field, desc := range v.ErrorsDescription() {
+		fmt.Fprintf(buf, "%s: %s\n", field, desc)
+	}
+
+	return buf.String()
+}
 
 var (
 	m       sync.Mutex
-	schemas map[string]*jsonschema.Schema
+	schemas map[string]*gojsonschema.Schema
 )
 
 // AddSchemaDir receives a path to a directory that contains only valid JSON schema
@@ -34,18 +67,29 @@ func AddSchemaDir(dirname string) error {
 		return fmt.Errorf("error reading files from %s: %v", dirname, err)
 	}
 
-	schemas = map[string]*jsonschema.Schema{}
+	schemas = map[string]*gojsonschema.Schema{}
 	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
 		filename := file.Name()
 
 		if schemas[filename] != nil {
 			return fmt.Errorf("schema key conflict: JSON schema %s already exists", filename)
 		}
 
-		schemas[filename], err = jsonschema.Compile(path.Join(dirname, filename))
+		bytes, err := ioutil.ReadFile(path.Join(dirname, filename))
+		if err != nil {
+			return fmt.Errorf("error reading file %s: %v", file.Name(), err)
+		}
+
+		schema, err := gojsonschema.NewSchema(gojsonschema.NewBytesLoader(bytes))
 		if err != nil {
 			return fmt.Errorf("error compiling JSON schema %s: %v", filename, err)
 		}
+
+		schemas[filename] = schema
 	}
 
 	return nil
@@ -66,9 +110,28 @@ func Schemas() []string {
 // of the reader comply with the given schema definition. If the schema name
 // does not exists, then an error is returned.
 func Validate(schemaName string, r io.Reader) error {
-	if schema, ok := schemas[schemaName]; ok {
-		return schema.Validate(r)
+	bytes, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	loader := gojsonschema.NewBytesLoader(bytes)
+
+	schema, exists := schemas[schemaName]
+	if !exists {
+		return fmt.Errorf("JSON schema %s was not found", schemaName)
 	}
 
-	return fmt.Errorf("JSON schema %s was not found", schemaName)
+	res, err := schema.Validate(loader)
+	if err != nil {
+		return fmt.Errorf("error validating JSON through the schema: %v", err)
+	}
+
+	if res.Valid() {
+		return nil
+	}
+
+	return &ValidationError{
+		Schema: schemaName,
+		Errors: res.Errors(),
+	}
 }
